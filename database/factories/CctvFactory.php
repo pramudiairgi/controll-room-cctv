6bb55a85c688fe4907bbe58b5326d6a41400fa45
pramudiairgi <gyxpram@gmail.com>
2026-06-04 14:22:35 +0700

    fix import enum

--- database/factories/CctvFactory.php
+++ database/factories/CctvFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\AssetCategory;
+use App\Enums\CctvStatus;
 use App\Models\Cctv;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -19,12 +21,12 @@ class CctvFactory extends Factory
     {
         return [
             'name' => $this->faker->sentence(3),
-            'category' => $this->faker->randomElement(array_values(App\Enums\AssetCategory::cases()))->value,
+            'category' => $this->faker->randomElement(array_values(AssetCategory::cases()))->value,
             'location' => $this->faker->address,
             'latitude' => $this->faker->latitudeBetween(-7.1, -6.8),
             'longitude' => $this->faker->longitudeBetween(110.35, 110.72),
             'stream_id' => $this->faker->bothify('???????????'),
-            'status' => $this->faker->randomElement(array_values(App\Enums\CctvStatus::cases()))->value,
+            'status' => $this->faker->randomElement(array_values(CctvStatus::cases()))->value,
             'notes' => $this->faker->sentence,
         ];
     }
