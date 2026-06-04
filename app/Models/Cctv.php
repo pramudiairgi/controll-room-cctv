85d34a230d5f0661fbe10e8f60fe1a03d0a84277
pramudiairgi <gyxpram@gmail.com>
2026-06-04 14:36:37 +0700

    fix: enable API routes and add missing Sanctum/YouTube support

--- app/Models/Cctv.php
+++ app/Models/Cctv.php
@@ -28,4 +28,21 @@ class Cctv extends Model
         'latitude' => 'decimal:7',
         'longitude' => 'decimal:7',
     ];
+
+    public static function extractYouTubeId(?string $url): ?string
+    {
+        if (empty($url)) {
+            return null;
+        }
+
+        if (preg_match('/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/', $url, $matches)) {
+            return $matches[1];
+        }
+
+        if (preg_match('/^[^"&?\/\s]{11}$/', $url)) {
+            return $url;
+        }
+
+        return null;
+    }
 }
