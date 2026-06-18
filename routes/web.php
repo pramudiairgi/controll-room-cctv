70dfd515b040692a67878c9ef4640e6f8425db96
pramudiairgi <gyxpram@gmail.com>
2026-06-18 15:18:35 +0700

    fix: add login route to web.php for Sanctum auth redirect

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function () {
+    return response()->file(public_path('login/index.html'));
+})->name('login');
+
 Route::get('/{any?}', function () {
     return response()->file(public_path('index.html'));
 })->where('any', '.*');
